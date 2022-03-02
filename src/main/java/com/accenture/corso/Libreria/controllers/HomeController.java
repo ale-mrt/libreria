ca36a97513e34a17130a7097a8af948eaa18f955
Alessandro Murtas <alessandro.murtas1@gmail.com>
2022-03-02 10:59:13 +0100

    added html navigation stuff and controller functionality

--- src/main/java/com/accenture/corso/Libreria/controllers/HomeController.java
+++ src/main/java/com/accenture/corso/Libreria/controllers/HomeController.java
@@ -1,8 +1,32 @@
 package com.accenture.corso.Libreria.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
+	@GetMapping({"", "/", "home.html", "home", "index.html", "index"})
+	public String home() {
+		return "pages/home.html";
+	}
+	
+	@GetMapping({"create.html", "create"})
+	public String create() {
+		return "pages/create.html";
+	}
+	
+	@GetMapping({"read.html", "read"})
+	public String read() {
+		return "pages/read.html";
+	}
+	
+	@GetMapping({"update.html", "update"})
+	public String update() {
+		return "pages/update.html";
+	}
+	
+	@GetMapping({"delete.html", "delete"})
+	public String delete() {
+		return "pages/home.html";
+	}
 }
